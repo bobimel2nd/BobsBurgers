c7cbfc6a46db3cb1b243b1949d91eb26d9eb7bc4
bobimel2nd <31457563+bobimel2nd@users.noreply.github.com>
2018-02-19 13:29:22 -0800

    Version 1.1.3

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,16 @@ var myLocalHost = {
   password: "(root)",
   database: "burgers_db"
 }
+var myRemoteHost = {
+  host: "gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "rt6qvjiml0tip8vb",
+  password: "kszp9mljsftkk1v9",
+  database: "eblxhenbsa13yhe9"
+}
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL || myLocalHost);
+// var connection = mysql.createConnection(myLocalHost);
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(myRemoteHost);
 
 connection.connect(function(err) {
   if (err) {
